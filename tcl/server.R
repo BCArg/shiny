96625ddc5624d6964e9cc20e11f52a51c0995f01
Gregoire Vincke <gregoire.vincke@ataya.be>
2013-11-27 09:56:33 +0100

    Supprimer les fonctions d'appel des paramètres et les remplacer par le input correspondants, car ils sont déjà eux même réactifs.

--- tcl/server.R
+++ tcl/server.R
@@ -13,31 +13,20 @@ shinyServer(function(input, output){
 #pour créer les graphiques des distributions théoriques : 
   # axe des X
   X = seq(0,20, length = 1000) 
-  # paramètres sur lesquels on pourra jouer (conditionnellement à la distribution considérée) 
-  pN1 <-reactive({if (input$dist == "DN") return (input$mean)}) #paramètres de la Normale
-  pN2 <-reactive({if (input$dist == "DN") return (input$sd)})
-  pU1 <-reactive({if (input$dist == "DU") return (input$a)})    #paramètres de l'Uniforme
-  pU2 <-reactive({if (input$dist == "DU") return (input$b)})
-  pC  <-reactive({if (input$dist == "DC") return (input$df)})    #paramètre de la Chi-carrée
-  pF1 <- reactive({if (input$dist == "DF") return (input$df1)})  #paramètres de la Fisher
-  pF2 <- reactive({if (input$dist == "DF") return (input$df2)})
-  pE  <- reactive({if (input$dist == "DE") return (input$rate)}) #paramètres de l'Exponentielle
-  pG1 <- reactive({if (input$dist == "DG") return (input$rate2)})#paramètres de la Gamma
-  pG2 <- reactive({if (input$dist == "DG") return (input$scale)})
   #densité Y    
   getY <-reactive({
     if (input$dist == "DN")
-      return(dnorm(X, mean = pN1(), sd = pN2()))
+      return(dnorm(X, mean = input$mean, sd = input$sd))
     if (input$dist == "DU")
-      return(dunif (X, min = pU1(), max = pU2()))
+      return(dunif (X, min = input$a, max = input$b))
     if (input$dist == "DC")
-      return (dchisq(X, df = pC()))
+      return (dchisq(X, df = input$df))
     if (input$dist == "DF")
-      return(df(X,df1 = pF1(),df2 = pF2()))
+      return(df(X,df1 = input$df1,df2 = input$df2))
     if (input$dist == "DE")
-      return (dexp(X, rate = pE()))
+      return (dexp(X, rate = input$rate))
     if (input$dist == "DG")
-      return(dgamma(X, shape = pG1(), rate = pG2()))
+      return(dgamma(X, shape = input$rate2, rate = input$scale))
     })
   
 
@@ -80,17 +69,17 @@ shinyServer(function(input, output){
     if(input$takeech != 0){
     
       if (input$dist == "DN")
-        return(rnorm(input$n, mean = pN1(), sd = pN2()))
+        return(rnorm(input$n, mean = input$mean, sd = input$sd))
       if (input$dist == "DU")
-        return(runif (input$n, min = pU1(), max = pU2()))
+        return(runif (input$n, min = input$a, max = input$b))
       if (input$dist == "DC")
-        return (rchisq(input$n, df = pC()))
+        return (rchisq(input$n, df = input$df))
       if (input$dist == "DF")
-        return(rf(input$n,df1 = pF1(),df2 = pF2()))
+        return(rf(input$n,df1 = input$df1,df2 = input$df2))
       if (input$dist == "DE")
-        return (rexp(input$n, rate = pE()))
+        return (rexp(input$n, rate = input$rate))
       if (input$dist == "DG")
-        return(rgamma(input$n, shape = pG1(), rate = pG2()))}
+        return(rgamma(input$n, shape = input$rate2, rate = input$scale))}
        
       })
   
