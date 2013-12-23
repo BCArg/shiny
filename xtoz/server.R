a571e78607259fea53be448081d90152f4ac3ecd
Gregoire Vincke <gregoire.vincke@ataya.be>
2013-12-23 09:59:42 +0100

    Add tabs for one value or interval, and get computed values treated out of plots

--- xtoz/server.R
+++ xtoz/server.R
@@ -17,143 +17,181 @@ y=dnorm(z) #créer pour chaque z les équivalents y de la courbe de gauss
 #Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
  
-output$zPlot <- renderPlot({
-  vx<-input$x
-  mx<-input$mx
-  sx<-input$sx
-  sens<-input$sens
-  
-  if(sens=="inf"){
-    z1=-5
-    z2=round((vx-mx)/sx,2)
-  }
-  if(sens=="sup"){
-    z2=5
-    z1=round((vx-mx)/sx,2)
-  }
-  if(sens=="equal"){
-    z2=round((vx-mx)/sx,2)
-    z1=round((vx-mx)/sx,2)
-  }
-  
-  z1lp=ifelse(z1 < -4,-4,z1) # limite la valeur de z à -4 pour le tracé du polugone sinon il overwrite le pointillé
-	z2lp=ifelse(z2 > 4,4,z2) # limite la valeur de z à 4 pour le tracé du polugone sinon il overwrite le pointillé
-	xlp=seq(z1lp,z2lp,length=200) #crée une seconde abscisse de -2 à 2
-	ylp=dnorm(xlp) #crée pour chaque xles équivalent y Normaux
-	
-	par(mai=c(1.02,0.8,0.9,1.5)) #marges bottom left top right en inches les valeurs par défaut sont  1 1 1 1 
-	plot(z,y,type="l",lwd=2,col="blue",yaxt="n",bty="n",,las=1,xaxs="i",yaxs="i",cex.lab=1,cex.axis=1.5,xlim=c(-5,plotzmaxlim),ylim=c(0,0.4),xaxp=c(-4,4,8),ylab="density",xlab="") #trace une courbe a partir de tous les couples x;y, et la colore en rouge. bty : A character string which determined the type of box which is drawn about plots. If bty is one of "o" (the default), "l", "7", "c", "u", or "]" the resulting box resembles the corresponding upper case letter. A value of "n" suppresses the box. xaxt="n" = pas dessiner axe des x
-	axis(2,las=2)#col="red",col.axis="blue",font.axis=2
-	lines(x=c(-4.7,-4),y = c(0,0),lty=2)
-	lines(x=c(4.7,4),y = c(0,0),lty=2)
-	polygon(c(z1lp,xlp,z2lp),c(0,ylp,0),col="lightblue") #trace un polygone qui commence à -2;0 puis x;y puis 2;0 et comme c'est polygon il ferme entre 2;0 et -2;0
-  mtext(plotzlab,side=1,line=1,at=5,cex=ztxt,adj = 0)
-  #Ecrire valeurs de x sur abscisse
-  for(s in -4:4){
-    vx=mx+s*sx
-    mtext(vx,side=1,line=linexlab,at=s,cex=1.5) 
-  }
-  mtext(paste("X~N(",mx,";",sx^2,")",sep=""),side=1,line=linexlab,at=5,cex=1.2,adj = 0)
- 
-  pz1=pnorm(z1,mean=0,sd=1)
-  pz1r=round(pz1,4)
-  pz1i=1-pz1
-  pz1ir=1-pz1r#round(pz1i,4)
-  pz2=pnorm(z2,mean=0,sd=1)
-  pz2r=round(pz2,4)
-  pz2i=1-pz2
-  pz2ir=1-pz2r#round(pz2i,4)
-  p=pz2-pz1
-  pr=pz2r-pz1r#round(p,4)
-  pp=round(pr*100,0)
-  z1l=ifelse(z1 < -3,-3,z1) # limite la valeur de z à -3 pour le calcul de la position de
-  z2l=ifelse(z2 > 3,3,z2) # limite la valeur de z à 3 pour le calcul de la position dep
-  if(sens=="inf"){
-    zx=(max(z2l,z1l)-((max(z2l,z1l)-min(z2l,z1l))/3))
-  }
-  if(sens=="sup"){
-    zx=(min(z2l,z1l)+((max(z2l,z1l)-min(z2l,z1l))/3))
-  }
-  if(sens=="equal"){
-    zx=z2l
-  }
-  text(zx,0.1,pr,cex=2)#cex = zoom taille texte
+  getInputValues<-reactive({
+    return(input)#collect all inputs
   })
+
+  getComputedValues<-reactive({
+    v<-getInputValues() # get all values of input list
+    cv<-list()#created empty computed values list
+    
+    if(v$Tabset==1){
+      if(v$sens=="inf"){
+	cv$z1=-5
+	cv$z2=round((v$x-v$mx)/v$sx,2)
+      }
+      if(v$sens=="sup"){
+	cv$z2=5
+	cv$z1=round((v$x-v$mx)/v$sx,2)
+      }
+      if(v$sens=="equal"){
+	cv$z2=round((v$x-v$mx)/v$sx,2)
+	cv$z1=round((v$x-v$mx)/v$sx,2)
+      }
+    }
+    
+    if(v$Tabset==2){
+      cv$x1=min(v$x1,v$x2)
+      cv$x2=max(v$x1,v$x2)
+      cv$z1=round((cv$x1-v$mx)/v$sx,2)
+      cv$z2=round((cv$x2-v$mx)/v$sx,2)
+    }
+    
+    cv$z1l=ifelse(cv$z1 < -3,-3,cv$z1) # limite la valeur de z à -3 pour le calcul de la position de
+    cv$z2l=ifelse(cv$z2 > 3,3,cv$z2) # limite la valeur de z à 3 pour le calcul de la position dep
+  
+    cv$z1lp=ifelse(cv$z1 < -4,-4,cv$z1) # limite la valeur de z à -4 pour le tracé du polugone sinon il overwrite le pointillé
+    cv$z2lp=ifelse(cv$z2 > 4,4,cv$z2) # limite la valeur de z à 4 pour le tracé du polugone sinon il overwrite le pointillé
+    cv$xlp=seq(cv$z1lp,cv$z2lp,length=200) #crée une seconde abscisse de -2 à 2
+    cv$ylp=dnorm(cv$xlp) #crée pour chaque xles équivalent y Normaux
+	
+    cv$pz1=pnorm(cv$z1,mean=0,sd=1)
+    cv$pz1r=round(cv$pz1,4)
+    cv$pz1i=1-cv$pz1
+    cv$pz1ir=1-cv$pz1r#round(pz1i,4)
+    
+    cv$pz2=pnorm(cv$z2,mean=0,sd=1)
+    cv$pz2r=round(cv$pz2,4)
+    cv$pz2i=1-cv$pz2
+    cv$pz2ir=1-cv$pz2r#round(pz2i,4)
+    
+    cv$p=cv$pz2-cv$pz1
+    cv$pr=cv$pz2r-cv$pz1r#round(p,4)
+    cv$pp=round(cv$pr*100,0)
+  
+    if(v$Tabset==1){
+      if(v$sens=="inf"){
+	cv$zx=(max(cv$z2l,cv$z1l)-((max(cv$z2l,cv$z1l)-min(cv$z2l,cv$z1l))/3))
+      }
+      if(v$sens=="sup"){
+	cv$zx=(min(cv$z2l,cv$z1l)+((max(cv$z2l,cv$z1l)-min(cv$z2l,cv$z1l))/3))
+      }
+      if(v$sens=="equal"){
+	cv$zx=cv$z2l
+      }
+    }
+    
+    if(v$Tabset==2){
+      cv$zx=(cv$z2l-(cv$z2l-cv$z1l)/2)
+    }
+    
+    return(cv)
+  })
+  
+  output$zPlot <- renderPlot({
+    v<-getInputValues()
+    cv<-getComputedValues()
+        if(v$Tabset==1){
+    par(mai=c(1.02,0.8,0.9,1.5)) #marges bottom left top right en inches les valeurs par défaut sont  1 1 1 1 
+    plot(z,y,type="l",lwd=2,col="blue",yaxt="n",bty="n",,las=1,xaxs="i",yaxs="i",cex.lab=1,cex.axis=1.5,xlim=c(-5,plotzmaxlim),ylim=c(0,0.4),xaxp=c(-4,4,8),ylab="density",xlab="")
+    axis(2,las=2)#col="red",col.axis="blue",font.axis=2
+    lines(x=c(-4.7,-4),y = c(0,0),lty=2)
+    lines(x=c(4.7,4),y = c(0,0),lty=2)
+    polygon(c(cv$z1lp,cv$xlp,cv$z2lp),c(0,cv$ylp,0),col="lightblue") #trace un polygone qui commence à -2;0 puis x;y puis 2;0 et comme c'est polygon il ferme entre 2;0 et -2;0
+    mtext(plotzlab,side=1,line=1,at=5,cex=ztxt,adj = 0)
+    for(s in -4:4){#Ecrire valeurs de x sur abscisse
+      vx=v$mx+s*v$sx
+      mtext(vx,side=1,line=linexlab,at=s,cex=1.5) 
+    }
+    mtext(paste("X~N(",v$mx,";",v$sx^2,")",sep=""),side=1,line=linexlab,at=5,cex=1.2,adj = 0)
+    text(cv$zx,0.1,cv$pr,cex=2)#cex = zoom taille texte
+    }
+    })
+ 
+  output$zPlotInt <- renderPlot({
+    v<-getInputValues()
+    cv<-getComputedValues()
+    if(v$Tabset==2){
+    par(mai=c(1.02,0.8,0.9,1.5)) #marges bottom left top right en inches les valeurs par défaut sont  1 1 1 1 
+    plot(z,y,type="l",lwd=2,col="blue",yaxt="n",bty="n",,las=1,xaxs="i",yaxs="i",cex.lab=1,cex.axis=1.5,xlim=c(-5,plotzmaxlim),ylim=c(0,0.4),xaxp=c(-4,4,8),ylab="density",xlab="") 
+    axis(2,las=2)#col="red",col.axis="blue",font.axis=2
+    lines(x=c(-4.7,-4),y = c(0,0),lty=2)
+    lines(x=c(4.7,4),y = c(0,0),lty=2)
+    polygon(c(cv$z1lp,cv$xlp,cv$z2lp),c(0,cv$ylp,0),col="lightblue") #trace un polygone qui commence à -2;0 puis x;y puis 2;0 et comme c'est polygon il ferme entre 2;0 et -2;0
+    mtext(plotzlab,side=1,line=1,at=5,cex=ztxt,adj = 0)
+    for(s in -4:4){#Ecrire valeurs de x sur abscisse
+      vx=v$mx+s*v$sx
+      mtext(vx,side=1,line=linexlab,at=s,cex=1.5) 
+    }
+    mtext(paste("X~N(",v$mx,";",v$sx^2,")",sep=""),side=1,line=linexlab,at=5,cex=1.2,adj = 0)
+    text(cv$zx,0.1,cv$pr,cex=2)#cex = zoom taille texte
+    }
+    })
+ 
  
   output$Text <- renderText({
-    vx<-input$x
-    mx<-input$mx
-    sx<-input$sx
-    sens<-input$sens
-    
-    if(sens=="inf"){
-      z1=-5
-      z2=round((vx-mx)/sx,2)
-    }
-    if(sens=="sup"){
-      z2=5
-      z1=round((vx-mx)/sx,2)
-    }
-    if(sens=="equal"){
-      z2=round((vx-mx)/sx,2)
-      z1=round((vx-mx)/sx,2)
-    }
-    pz1=pnorm(z1,mean=0,sd=1)
-    pz1r=round(pz1,4)
-    pz1i=1-pz1
-    pz1ir=1-pz1r#round(pz1i,4)
-    pz2=pnorm(z2,mean=0,sd=1)
-    pz2r=round(pz2,4)
-    pz2i=1-pz2
-    pz2ir=1-pz2r#round(pz2i,4)
-    p=pz2-pz1
-    pr=pz2r-pz1r#round(p,4)
-    pp=round(pr*100,0)
-    #txt=paste("<p>z1=",z1,";z2=",z2,"</p>",sep="")
-    txt=paste("<p>Si X est une variable al&eacute;atoire Normale X~N(&mu;;&sigma;&sup2;) telle que X~N(",mx,";",sx^2,") alors Z=(X-&mu;)/&sigma; est aussi une variable al&eacute;atoire Normale de param&egrave;tres Z~N(0;1) ",sep="")
-    if(sens=="inf" & z2>-4 & z2<0){
+    v<-getInputValues()
+    cv<-getComputedValues()
+
+    txt=paste("<p>Si X est une variable al&eacute;atoire Normale X~N(&mu;;&sigma;&sup2;) telle que X~N(",v$mx,";",v$sx^2,") alors Z=(X-&mu;)/&sigma; est aussi une variable al&eacute;atoire Normale de param&egrave;tres Z~N(0;1) ",sep="")
+    if(v$sens=="inf" & cv$z2>-4 & cv$z2<0){
       txt=paste(txt," et P(X&le;x) = P(Z&le;z).","</p>",sep="")
-      txt=paste(txt,"<p>Si x=",vx,", alors z = (x-&mu;)/&sigma; = (",vx,"-",mx,")/",sx," = ",vx-mx,"/",sx," = ",z2,"</p>",sep="")
-      txt=paste(txt,"<p>Donc P(X&le;",round(z2*sx+mx,2),") = P(Z&le;",z2,") = 1-P(Z&le;",z2*-1,") = 1-",pz2ir," = ",pz2r,"</p>",sep="")
+      txt=paste(txt,"<p>Si x=",v$x,", alors z = (x-&mu;)/&sigma; = (",v$x,"-",v$mx,")/",v$sx," = ",v$x-v$mx,"/",v$sx," = ",cv$z2,"</p>",sep="")
+      txt=paste(txt,"<p>Donc P(X&le;",round(cv$z2*v$sx+v$mx,2),") = P(Z&le;",cv$z2,") = 1-P(Z&le;",cv$z2*-1,") = 1-",cv$pz2ir," = ",cv$pz2r,"</p>",sep="")
     }
-    if(sens=="inf" & z2<4 & z2>=0){
+    if(v$sens=="inf" & cv$z2<4 & cv$z2>=0){
       txt=paste(txt," et P(X&le;x) = P(Z&le;z).","</p>",sep="")
-      txt=paste(txt,"<p>Si x=",vx,", alors z = (x-&mu;)/&sigma; = (",vx,"-",mx,")/",sx," = ",vx-mx,"/",sx," = ",z2,"</p>",sep="")
-      txt=paste(txt,"<p>Donc P(X&le;",round(z2*sx+mx,2),") = P(Z&le;",z2,") = ",pz2r,"</p>",sep="")
+      txt=paste(txt,"<p>Si x=",v$x,", alors z = (x-&mu;)/&sigma; = (",v$x,"-",v$mx,")/",v$sx," = ",v$x-v$mx,"/",v$sx," = ",cv$z2,"</p>",sep="")
+      txt=paste(txt,"<p>Donc P(X&le;",round(cv$z2*v$sx+v$mx,2),") = P(Z&le;",cv$z2,") = ",cv$pz2r,"</p>",sep="")
     }
-    if(z1>-4 & z1<0 & sens=="sup"){
+    if(cv$z1>-4 & cv$z1<0 & v$sens=="sup"){
       txt=paste(txt," et P(X&ge;x) = P(Z&ge;z).","</p>",sep="")
-      txt=paste(txt,"<p>Si x=",vx,", alors z = (x-&mu;)/&sigma; = (",vx,"-",mx,")/",sx," = ",vx-mx,"/",sx," = ",z1,"</p>",sep="")
-      txt=paste(txt,"<p>Donc P(X&ge;",round(z1*sx+mx,2),") = P(Z&ge;",z1,") = P(Z&le;",z1*-1,") = ",pz1ir,"</p>",sep="")
+      txt=paste(txt,"<p>Si x=",v$x,", alors z = (x-&mu;)/&sigma; = (",v$x,"-",v$mx,")/",v$sx," = ",v$x-v$mx,"/",v$sx," = ",cv$z1,"</p>",sep="")
+      txt=paste(txt,"<p>Donc P(X&ge;",round(cv$z1*v$sx+v$mx,2),") = P(Z&ge;",cv$z1,") = P(Z&le;",cv$z1*-1,") = ",cv$pz1ir,"</p>",sep="")
     }
-    if(z1<4 & z1>=0 & sens=="sup"){
+    if(cv$z1<4 & cv$z1>=0 & v$sens=="sup"){
       txt=paste(txt," et P(X&ge;x) = P(Z&ge;z).","</p>",sep="")
-      txt=paste(txt,"<p>Si x=",vx,", alors z = (x-&mu;)/&sigma; = (",vx,"-",mx,")/",sx," = ",vx-mx,"/",sx," = ",z1,"</p>",sep="")
-      txt=paste(txt,"<p>Donc P(X&ge;",round(z1*sx+mx,2),") = P(Z&ge;",z1,") = 1-P(Z&le;",z1,") = 1-",pz1r," = ",pz1ir,"</p>",sep="")
+      txt=paste(txt,"<p>Si x=",v$x,", alors z = (x-&mu;)/&sigma; = (",v$x,"-",v$mx,")/",v$sx," = ",v$x-v$mx,"/",v$sx," = ",cv$z1,"</p>",sep="")
+      txt=paste(txt,"<p>Donc P(X&ge;",round(cv$z1*v$sx+v$mx,2),") = P(Z&ge;",cv$z1,") = 1-P(Z&le;",cv$z1,") = 1-",cv$pz1r," = ",cv$pz1ir,"</p>",sep="")
     }
-    if(sens=="inf" & z2>=4){
+    if(v$sens=="inf" & cv$z2>=4){
       txt=paste(txt," et P(X&le;x) = P(Z&le;z).","</p>",sep="")
-      txt=paste(txt,"<p>Si x=",vx,", alors z = (x-&mu;)/&sigma; = (",vx,"-",mx,")/",sx," = ",vx-mx,"/",sx," = ",z2,"</p>",sep="")
-      txt=paste(txt,"<p>Donc P(X&le;",round(z2*sx+mx,2),") = P(Z&le;",z2,") = P(Z&le;&infin;) = ",pz2r,"</p>",sep="")
+      txt=paste(txt,"<p>Si x=",v$x,", alors z = (x-&mu;)/&sigma; = (",v$x,"-",v$mx,")/",v$sx," = ",v$x-v$mx,"/",v$sx," = ",cv$z2,"</p>",sep="")
+      txt=paste(txt,"<p>Donc P(X&le;",round(cv$z2*v$sx+v$mx,2),") = P(Z&le;",cv$z2,") = P(Z&le;&infin;) = ",cv$pz2r,"</p>",sep="")
     }
-    if(sens=="inf" & z2<=-4){
+    if(v$sens=="inf" & cv$z2<=-4){
       txt=paste(txt," et P(X&le;x) = P(Z&le;z).","</p>",sep="")
-      txt=paste(txt,"<p>Si x=",vx,", alors z = (x-&mu;)/&sigma; = (",vx,"-",mx,")/",sx," = ",vx-mx,"/",sx," = ",z2,"</p>",sep="")
-      txt=paste(txt,"<p>Donc P(X&le;",round(z2*sx+mx,2),") = P(Z&le;",z2,") = 1-P(Z&le;",z2*-1,") = 1-P(Z&le;&infin;) = 1-1 = ",pz2r,"</p>",sep="")
+      txt=paste(txt,"<p>Si x=",v$x,", alors z = (x-&mu;)/&sigma; = (",v$x,"-",v$mx,")/",v$sx," = ",v$x-v$mx,"/",v$sx," = ",cv$z2,"</p>",sep="")
+      txt=paste(txt,"<p>Donc P(X&le;",round(cv$z2*v$sx+v$mx,2),") = P(Z&le;",cv$z2,") = 1-P(Z&le;",cv$z2*-1,") = 1-P(Z&le;&infin;) = 1-1 = ",cv$pz2r,"</p>",sep="")
     }
-    if(z1<=-4 & sens=="sup"){
+    if(cv$z1<=-4 & v$sens=="sup"){
       txt=paste(txt," et P(X&ge;x) = P(Z&ge;z).","</p>",sep="")
-      txt=paste(txt,"<p>Si x=",vx,", alors z = (x-&mu;)/&sigma; = (",vx,"-",mx,")/",sx," = ",vx-mx,"/",sx," = ",z1,"</p>",sep="")
-      txt=paste(txt,"<p>Donc P(X&ge;",round(z1*sx+mx,2),") = P(Z&ge;",z1,") = 1-P(Z&le;",z1*-1,") = 1-P(Z&le;&infin;) = 1-1 = ",pz1r,"</p>",sep="")
+      txt=paste(txt,"<p>Si x=",v$x,", alors z = (x-&mu;)/&sigma; = (",v$x,"-",v$mx,")/",v$sx," = ",v$x-v$mx,"/",v$sx," = ",cv$z1,"</p>",sep="")
+      txt=paste(txt,"<p>Donc P(X&ge;",round(cv$z1*v$sx+v$mx,2),") = P(Z&ge;",cv$z1,") = 1-P(Z&le;",cv$z1*-1,") = 1-P(Z&le;&infin;) = 1-1 = ",cv$pz1r,"</p>",sep="")
     }
-    if(z1>=4 & sens=="sup"){
+    if(cv$z1>=4 & v$sens=="sup"){
       txt=paste(txt," et P(X&ge;x) = P(Z&ge;z).","</p>",sep="")
-      txt=paste(txt,"<p>Si x=",vx,", alors z = (x-&mu;)/&sigma; = (",vx,"-",mx,")/",sx," = ",vx-mx,"/",sx," = ",z1,"</p>",sep="")
-      txt=paste(txt,"<p>Donc P(X&ge;",round(z1*sx+mx,2),") = P(Z&ge;",z1,") = 1-P(Z&le;",z1,") = 1-P(Z&le;&infin;) = 1-1 = ",pz1r,"</p>",sep="")
+      txt=paste(txt,"<p>Si x=",v$x,", alors z = (x-&mu;)/&sigma; = (",v$x,"-",v$mx,")/",v$sx," = ",v$x-v$mx,"/",v$sx," = ",cv$z1,"</p>",sep="")
+      txt=paste(txt,"<p>Donc P(X&ge;",round(cv$z1*v$sx+v$mx,2),") = P(Z&ge;",cv$z1,") = 1-P(Z&le;",cv$z1,") = 1-P(Z&le;&infin;) = 1-1 = ",cv$pz1r,"</p>",sep="")
     }
-    if(sens=="equal"){
+    if(v$sens=="equal"){
       txt=paste(txt," et P(X=x) = P(Z=z) = 0.","</p>",sep="")
     }
+    paste(txt)
+  })
+  
+  output$TextInt <- renderText({
+    v<-getInputValues()
+    cv<-getComputedValues()
+
+    txt=paste("<p>Si X est une variable al&eacute;atoire Normale X~N(&mu;;&sigma;&sup2;) telle que X~N(",v$mx,";",v$sx^2,") alors Z=(X-&mu;)/&sigma; est aussi une variable al&eacute;atoire Normale de param&egrave;tres Z~N(0;1) </p>",sep="")
+
+    txt=paste(txt,"<p>P(x<sub>1</sub> &le; X &le; x<sub>2</sub>) = P(z<sub>1</sub> &le; Z &le; z<sub>2</sub>).","</p>",sep="")
+    txt=paste(txt,"<p>Si x<sub>1</sub>=",v$x1,", alors z<sub>1</sub> = (x<sub>1</sub>-&mu;)/&sigma; = (",v$x1,"-",v$mx,")/",v$sx," = ",v$x1-v$mx,"/",v$sx," = ",cv$z1,"</p>",sep="")
+    txt=paste(txt,"<p>Si x<sub>2</sub>=",v$x2,", alors z<sub>2</sub> = (x<sub>2</sub>-&mu;)/&sigma; = (",v$x2,"-",v$mx,")/",v$sx," = ",v$x2-v$mx,"/",v$sx," = ",cv$z2,"</p>",sep="")
+    txt=paste(txt,"<p>Donc P(",cv$x1," &le; X &le; ",cv$x2,") = P(",cv$z1," &le; Z &le; ",cv$z2,") = P(Z &le; ",cv$z1,")-P(Z &le; ",cv$z2,") = ",cv$pz1ir,"-",cv$pz2ir," = ",cv$pr,"</p>",sep="")
+
+
     paste(txt)
   })
 
--- xtoz/ui.R
+++ xtoz/ui.R
@@ -7,20 +7,53 @@ shinyUI(pageWithSidebar(
   headerPanel("Probabilité associée à une valeur de X~N(µ;σ²)"),
 
   sidebarPanel(
+  
+    tags$head(
+        tags$style(type="text/css", "label { display: inline; }"),
+        tags$style(type="text/css", '.checkbox input[type="checkbox"],.radio input[type="radio"] { float: none; }'),
+        tags$style(type="text/css", 'input[type="number"] { width: 50px; }'),
+        tags$style(type="text/css", ".jslider { max-width: 350px; }"),
+        tags$style(type='text/css', ".well { max-width: 400px; }"),#class of the from inside sidebarPanel
+        tags$style(type='text/css', ".span4 { max-width: 400px; }")#span4 is the span of sidebarPanel (span8 is for the mainPanel)
+      ),
+  
     numericInput("mx", "µ :", 25),
+    br(),
     numericInput("sx", "σ :", 5),
-    numericInput("x", "X :", 30),
-    selectInput("sens", "Probabilité de type :",
-    list("P(X≤x)" = "inf", 
-	  "P(X≥x)" = "sup",
-	  "P(X=x)" = "equal")),
-    submitButton("Calculer la probabilité"),
+    br(),
+    conditionalPanel(condition = "input.Tabset==1",
+      numericInput("x", "X :", 30),
+      br(),
+      selectInput("sens", "Probabilité de type :",
+      list("P(X≤x)" = "inf", 
+	    "P(X≥x)" = "sup",
+	    "P(X=x)" = "equal"))
+    ),
+    conditionalPanel(condition = "input.Tabset==2",
+      numericInput("x1", HTML("X<sub>1</sub> :"), 20),
+      br(),
+      numericInput("x2", HTML("X<sub>2</sub> :"), 30)
+    ),
+
     HTML('<hr style="border:1px solid #ccc;"/>'),
     HTML('<a rel="license" href="http://creativecommons.org/licenses/by/2.0/be/"><img alt="Licence Creative Commons" style="border-width:0" src="http://i.creativecommons.org/l/by/2.0/be/80x15.png" /></a> Ce(tte) oeuvre de <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Statistical eLearning Tools</span> est mise à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by/2.0/be/">licence Creative Commons Attribution 2.0 Belgique</a>.'),
     HTML('<p>Détails sur l\'utilisation de cette ressource sur <a href="http://sites.uclouvain.be/selt/ressources/104033" target="_blank">Statistics eLearning Tools</a><br/> Code source disponible sur <a href="https://github.com/uclouvain-selt/shiny/tree/master/xtoz" target="_blank">Github</a></p>')
   ),
 
   mainPanel(
-	plotOutput("zPlot"),htmlOutput("Text")
+    tabsetPanel(id="Tabset",selected=2,
+      tabPanel(
+	"Une valeur",
+	plotOutput("zPlot"),
+	htmlOutput("Text"),
+	value=1
+      ),
+      tabPanel(
+	"Un intervalle",
+	plotOutput("zPlotInt"),
+	htmlOutput("TextInt"),
+	value=2
+      )
+    )
   )
 ))
