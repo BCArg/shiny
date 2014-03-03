0f798bae64a9ddc34b723d7fce6ada8b91bfe80c
Gregoire Vincke <gregoire.vincke@ataya.be>
2014-03-03 10:15:11 +0100

    Set seeicpcevolution to false

--- ic/ui.R
+++ ic/ui.R
@@ -23,7 +23,7 @@ shinyUI(pageWithSidebar(
     checkboxInput("seeicvarunknown","Voir l'intervalle de confiance pour µ quand σ² est inconnue", FALSE),
     checkboxInput("seeiconx","Voir la projection des limites des IC sur l'axe des abscisses", FALSE),
     #checkboxInput("seeconfidence","Identifier la zone de confiance dans la population d'origine", FALSE),
-    checkboxInput("seeicpcevolution","Voir l'évolution du % d'IC qui couvrent µ", TRUE),
+    checkboxInput("seeicpcevolution","Voir l'évolution du % d'IC qui couvrent µ", FALSE),
     br(),
     actionButton("takeech","Echantillonner"),
     actionButton("reset","Reset"),
