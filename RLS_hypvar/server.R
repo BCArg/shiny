86767455e4e20b96989222a6b8dd238b18386c30
gemarchand <geraldine.marchand@uclouvain.be>
2014-04-02 11:46:09 +0200

    New version : first steps

--- RLS_hypvar/server.R
+++ RLS_hypvar/server.R
@@ -1,19 +1,71 @@
 #Sys.setlocale("LC_ALL", "fr_FR.UTF-8")#to be sure that accents in text will be allowed in plots
 library(shiny)
-#library(bstats)
-#library(lmtest)
+library(bstats)
+library(lmtest)
 
 
 shinyServer(function(input, output) {
 
+  X <- runif (n = 100, min = 0, max = 20)
+  
+  rv <- reactiveValues()  # Create a reactiveValues object, to let us use settable reactive values
+  
+  rv$last.Y.value<-0
+  rv$Y<-list()
+  
+  rv$lastAction <- 'none'# To start out, lastAction == NULL, meaning nothing clicked yet
+  
+  # An observe block for each button, to record that the action happened
+  observe({
+    if (input$takeY != 0) {
+      rv$lastAction <- 'takeY'
+    }
+  })
+  observe({
+    if (input$reset != 0) {
+      rv$lastAction <- 'reset'
+      rv$Y<-list()
+    }
+  })
+  
+  
+  
+  getY <- reactive({
+    
+    if(rv$lastAction == "takeY")
+    
+    #epsilon <- rnorm(n = 100, mean = 0, sd = sqrt(5 + input$alpha1*X^2))
+    
+      
+      return (
+      rv$Y <-input$intercept + input$beta1*X + rnorm(n = 100, mean = 0, sd = sqrt(input$alpha0*X^input$alpha1))
+      )
+  })
+  
+ #   getInputValues<-reactive({
+#      return(input)#collect all inputs
+ #   })
+    
+  #  getComputedValues<-reactive({
+  #    Y<-getY()
+            
+  #    v<-getInputValues() # get all values of input list
+  #    cv<-list()#created empty computed values list
+      
+  
+   # rv$last.Y.value<-v$takeY
+    #  return(cv)
+    
+     #})
+   
+  
   
 
 output$doublePlot1 <- renderPlot({
-  X <- runif (n = input$n, min = 0, max = 20)
-  epsilon <- rnorm(n = input$n, mean = 0, sd = sqrt(5 + input$alpha1*X^2))
-  
-  Y <- 2 + 2*X + epsilon  
-  
+  Y<-getY()
+  Y<-unlist(Y) 
+ # v<-getInputValues()
+ #  cv<-getComputedValues()
   
   par(mfrow = c(1,2))
 
@@ -28,78 +80,34 @@ output$doublePlot1 <- renderPlot({
 },height = 300)
  
 
-###Afficher la statistique et la p-valeur du test de Breusch-Pagan  et du test de White
-output$Test1<- renderTable({    
-  
-  X <- runif (n = input$n, min = 0, max = 10)
-  epsilon <- rnorm(n = input$n, mean = 0, sd = sqrt(5 + input$alpha1*X^2))
-  Y <- 2 + 2*X + epsilon 
-  
-  bp <- bptest(Y ~ X)
-  
-  res <- lm (Y ~ X)
-  white <- white.test(res)
-  
-  
-  
-  table <- matrix(nrow = 2, ncol = 2)
-  dimnames(table) = list(c("Statistique du test", "p-valeur"), c("Breusch-Pagan", "White"))
-  table[1,1] <- bp$statistic
-  table[1,2] <- white$statistic
-  table[2,1] <- bp$p.value
-  table[2,2] <- white$p.value
-  table
-}, digits=5  , height = getPlotHeight, width=full.plot.width) 
-
-
-
-output$doublePlot2 <- renderPlot({
-
-X <- runif (n = input$n, min = 0, max = 20)
-epsilon <- rnorm(n = input$n, mean = 0, sd = sqrt(5*X^input$alpha1))
-Y <- 2 + 2*X + epsilon
-
-par(mfrow = c(1,2))
-plot(X, Y, main = "Plot X-Y") #nuage de points
-res <- lm (Y ~ X)
-abline (res, col = "blue")
-
-resid <- unlist(res[2])
-plot (X, resid, main = "Plot des résidus en fonction de X") #résidus en fonction de X)
-abline (h = 0)
-
-
-
-
-}, height = 300)
-
-
-
-###Afficher la statistique et la p-valeur du test de Breusch-Pagan  et du test de White
-output$Test2<- renderTable({    
-  
-  X <- runif (n = input$n, min = 0, max = 20)
-  epsilon <- rnorm(n = input$n, mean = 0, sd = sqrt(5*X^input$alpha1))
-  Y <- 2 + 2*X + epsilon
-  
-  bp <- bptest(Y ~ X)
-  
-  res <- lm (Y ~ X)
-  white <- white.test(res)
-  
-  
-  
-  table <- matrix(nrow = 2, ncol = 2)
-  dimnames(table) = list(c("Statistique du test", "p-valeur"), c("Breusch-Pagan", "White"))
-  table[1,1] <- bp$statistic
-  table[1,2] <- white$statistic
-  table[2,1] <- bp$p.value
-  table[2,2] <- white$p.value
-  table
-
+  ###Afficher la statistique et la p-valeur du test de Breusch-Pagan  et du test de White
+  output$Test1<- 
     
-  }, digits=5  , height = getPlotHeight, width=full.plot.width) 
+    renderTable({   if(input$Test=="TRUE"){  
+    
+    Y<-getY()
+    Y<-unlist(Y) 
+    
+    bp <- bptest(Y ~ X)
+    
+    res <- lm (Y ~ X)
+    white <- white.test(res)
+    
+    
+    
+    table <- matrix(nrow = 2, ncol = 2)
+    dimnames(table) = list(c("Statistique du test", "p-valeur"), c("Breusch-Pagan", "White"))
+    table[1,1] <- bp$statistic
+    table[1,2] <- white$statistic
+    table[2,1] <- bp$p.value
+    table[2,2] <- white$p.value
+    table
 
+    }
+    }, digits=5  , height = getPlotHeight, width=full.plot.width) 
+  
+
+  
 
 
 })
--- RLS_hypvar/ui.R
+++ RLS_hypvar/ui.R
@@ -1,34 +1,50 @@
 library(shiny)
 shinyUI(pageWithSidebar(
-  headerPanel("RÃ©gression linÃ©aire : violation de l'hypothÃ¨se d'homoscÃ©dasticitÃ©"), 
+  headerPanel("Régression linéaire : violation de l'hypothèse d'homoscédasticité"), 
   sidebarPanel(
-    sliderInput("n", "n", min = 50, max =400, value = 100, step = 5)
-  #  , sliderInput("alpha0", "alpha0", min = 0, max = 10, value = 1, step = 0.1)
+   # sliderInput("n", "n", min = 50, max =400, value = 100, step = 5)
+  
+    conditionalPanel(condition = "input.Tabset==1",     
+    h5("Paramètres de la RLS")
+    ,sliderInput("intercept", "Beta0", min = 0, max = 10, value = 0)
+    ,sliderInput("beta1", "Beta1", min = 0, max = 10, value = 0)
     
     
-    ,h5("Variance conditonnelle Ã  X: ")
-    , sliderInput("alpha1", "alpha1", min = 0, max = 5, value = 0, step = 0.01)
-  #  , sliderInput("intercept", "intercept", min = 0, max = 10, value = 0)
-  # , sliderInput(" beta1", " beta1", min = 0, max = 10, value = 2)
     
+    ,h5("Variance du terme d'erreur: ")
+    ,sliderInput("alpha0",  "Terme constant" , min = 0, max = 5, value = 2, step = 0.1)
+       ,sliderInput("alpha1", "Terme dépendant de X", min = 0, max = 5, value = 0, step = 0.01)
     
-    ,h5("RemÃ¨des: ")
-    ,checkboxInput("log",HTML("Transformation log"),FALSE)
+        
+    ,actionButton("takeY","Générer des données")
+    ,actionButton("reset","Reset")
+   
     
+    ,h5("Tests : hypothèse d'homoscédasticité")
+    ,checkboxInput("Test",HTML("Breusch-Pagan & White"),FALSE)
     
-    ), 
+   ),
+  
+  conditionalPanel(condition = "input.Tabset==2"
+                   
+                   # ,h5("RemÃ¨des: ")
+                   #  ,checkboxInput("log",HTML("Transformation log"),FALSE)
+  )
+    
+  ),
   
   mainPanel(
         
-    tabsetPanel(
-      tabPanel("Version1"
-               ,plotOutput("doublePlot1", height = "auto")
-               ,tableOutput("Test1") ), 
-      tabPanel("Version2" 
-               ,plotOutput("doublePlot2", height = "auto") 
-               ,tableOutput("Test2"))
-     
-      
+    tabsetPanel(id="Tabset",selected=1,
+      tabPanel(
+        "Violation de l'hypothèse",
+        plotOutput("doublePlot1", height = "auto"),
+        tableOutput("Test1"), 
+        value = 1), 
+      tabPanel(
+        "Remèdes", 
+        value = 2)
+          
       )
            )  
 ))
