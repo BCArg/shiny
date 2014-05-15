9b1c5bf360ad0ea81972cf47e6d084cfda84d4d7
Gregoire Vincke <gregoire.vincke@ataya.be>
2014-05-15 17:04:27 +0200

    HTML simplication of inputs (lateral panel) and outputs (main panel). Settings only in lateral panel, explanations in main panel

--- olsvarinf/ui.R
+++ olsvarinf/ui.R
@@ -17,32 +17,36 @@ headerPanel("Régression linéaire : conséquences de l'hétéroskédasticité p
 		tags$script(type="text/javascript",src="js/scripts.js")
 		),
     h5(HTML("Paramètres de l'échantillonnage"))
-      ,sliderInput("n", HTML("Nombre d'observations par échantillon : n"), min = 20, max = 500, value = 100, step = 5)
-      ,sliderInput("ns", HTML("Nombre d'échantillons prélevés par simulation"), min = 1, max = 50, value = 1, step = 1)
+      ,HTML("n : nombre d'individus par &eacute;chantillon&nbsp;:")
+      ,sliderInput("n","",min = 20,max = 500,value = 100, step=5)
+      ,HTML("Nombre d'&eacute;chantillons pr&eacute;lev&eacute;s&nbsp;:")
+      ,sliderInput("ns", "", min = 1, max = 50, value = 1, step = 1)
       ,p(HTML("k : Nombre total d'échantillons prélevés"))    
       
 
-    ,h5(HTML("Paramètres du modèle : Y = &beta;<sub>0</sub> + &beta;<sub>1</sub>X + &epsilon;"))
-      ,p(HTML("Intercept : &beta;<sub>0</sub> est fixé à 0"))
-      ,radioButtons("beta1", HTML("Pente : &nbsp; &nbsp; H<sub>0</sub> : &beta;<sub>1</sub> = 0 &nbsp; &nbsp; &nbsp;H<sub>1</sub> : &beta;<sub>1</sub> = 1"), 
-		c("H₀ est vraie"="h0",
-		  "H₁ est vraie"="h1"))#caractères unicodes copiés de http://fr.wikipedia.org/wiki/Exposants_et_indices_Unicode
-
-      
-      ,radioButtons("alpha1", HTML("Variance du terme d'erreur : Var(&epsilon;) = &sigma;<sup>2</sup> X<sup>&alpha;<sub>1</sub></sup>"), 
+#     ,h5(HTML("Modèle : Y = &beta;<sub>0</sub> + &beta;<sub>1</sub>X + &epsilon;"))
+,h5(HTML("Param&egrave;tres de l'inf&eacute;rence"))
+#       ,p(HTML("Pour simplifier &nbsp;: &beta;<sub>0</sub>=0; &beta;<sub>1</sub>=0 ou 1; &sigma;<sup>2</sup>=2."))
+     # ,p(HTML("&beta;<sub>0</sub> (intercept) : pour simplifier &beta;<sub>0</sub>=0. "))
+     # HTML("&beta;<sub>1</sub> (pente) : pour simplifier &beta;<sub>1</sub> = 0 ou 1&nbsp;:<br />")
+      ,selectInput("beta1","", 
+		c("H₀ : β₁ = 0 est vraie"="h0",
+		  "H₁ : β₁ = 1 est vraie"="h1"))#caractères unicodes copiés de http://fr.wikipedia.org/wiki/Exposants_et_indices_Unicode
+#       ,p(HTML("Variance d'&epsilon; = &sigma;<sup>2</sup> X<sup>&alpha;<sub>1</sub></sup>. Pour simplifier : &sigma;<sup>2</sup>=2&nbsp;: "))
+      ,selectInput("alpha1","", 
 		    c("Homoskédasticité : α₁ = 0"= "homo",
 		      "Hétéroskédasticité: α₁ ≠ 0" = "hetero"))
       
       ,conditionalPanel(condition = "input.alpha1 == 'hetero'"
 			,HTML("&alpha;<sub>1</sub>")
 			,sliderInput("V.alpha1","", min = 0.1, max = 10, value = 2, step = 0.1))
-      ,p(HTML("Le terme constant &sigma;<sup>2</sup>  est fixé à 2"))
-      
-      ,h5(HTML("Inférence"))
-      ,checkboxInput("barplot",HTML("Visualiser le % de RH<sub>0</sub>"),FALSE) 
-      ,p(HTML("<i><u>Si les hypothèses sur &epsilon; sont respectées :</u></i>")) 
-      ,p(HTML("- lorsque H<sub>0</sub> est vraie, le % de RH<sub>0</sub> converge vers le risque d'erreur de type I quand k &rarr; &infin; (le niveau de signification du test est fixé à 5%)"))    
-      ,p(HTML("- lorsque H<sub>1</sub> est vraie, le % de RH<sub>0</sub> converge vers la puissance qui &rarr;1 quand k &rarr; &infin;"))    
+      #,p(HTML("Le terme constant &sigma;<sup>2</sup>  est fixé à 2"))
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+#       ,h5(HTML("Inférence"))
+      ,br(),checkboxInput("barplot",HTML("Visualiser le % de RH<sub>0</sub>"),FALSE) 
+#       ,p(HTML("<i><u>Si les hypothèses sur &epsilon; sont respectées :</u></i>")) 
+#       ,p(HTML("- lorsque H<sub>0</sub> est vraie, le % de RH<sub>0</sub> converge vers le risque d'erreur de type I quand k &rarr; &infin; (le niveau de signification du test est fixé à 5%)"))    
+#       ,p(HTML("- lorsque H<sub>1</sub> est vraie, le % de RH<sub>0</sub> converge vers la puissance qui &rarr;1 quand k &rarr; &infin;")) 
 	      ## Graphic parameter  
 		,h5("Paramètres graphiques :")
 		,selectInput("display", "Display :",
@@ -56,10 +60,16 @@ headerPanel("Régression linéaire : conséquences de l'hétéroskédasticité p
   ),
 
   mainPanel(
-    HTML("<div id='mainInputs'>"),
-    actionButton("takesample","Echantillonner"),actionButton("reset","Reset"),checkboxInput("visM",HTML("Plein écran"),FALSE),
-    HTML("</div>"),
-    conditionalPanel(
+    HTML("<div id='mainInputs'>")
+    ,actionButton("takesample","Echantillonner"),actionButton("reset","Reset"),checkboxInput("visM",HTML("Plein écran"),FALSE)
+    ,HTML("</div>")
+    ,HTML('<hr style="border:1px solid #ccc;"/>')
+    ,h5(HTML("Mod&egrave;le th&eacute;orique g&eacute;n&eacute;ral : Y = &beta;<sub>0</sub> + &beta;<sub>1</sub>X + &epsilon;"))
+    ,p(HTML("Pour simplifier ce mod&egrave;le, dans le cadre de cet exemple les valeurs de l'intercept (&beta;<sub>0</sub>) et de la pente (&beta;<sub>1</sub>) ont &eacute;t&eacute; fix&eacute;es au valeurs  : &beta;<sub>0</sub>=0, et &beta;<sub>1</sub>=0 ou 1.<br />Donc quand &beta;<sub>0</sub>=0 et &beta;<sub>1</sub>=1 alors <strong>Y = X + &epsilon;</strong>, et quand &beta;<sub>0</sub>=0 et &beta;<sub>1</sub>=0 alors <strong>Y = &epsilon;</strong>.<br/>La variance du terme d'erreur est <strong>&sigma;<sub>&epsilon;</sub><sup>2</sup> = &sigma;<sup>2</sup> X<sup>&alpha;<sub>1</sub></sup></strong>. Pour simplifier ce calcul nous considerons que &sigma;<sup>2</sup>=2, donc : <strong>&sigma;<sub>&epsilon;</sub><sup>2</sup> = 2 X<sup>&alpha;<sub>1</sub></sup></strong>."))
+    ,h5(HTML("Inférence : <i><u>Si les hypothèses sur &epsilon; sont respectées :</u></i> :"))
+    ,p(HTML("- lorsque H<sub>0</sub> est vraie, le % de RH<sub>0</sub> converge vers le risque d'erreur de type I quand k &rarr; &infin; (le niveau de signification du test est fixé à 5%)"))    
+    ,p(HTML("- lorsque H<sub>1</sub> est vraie, le % de RH<sub>0</sub> converge vers la puissance qui &rarr;1 quand k &rarr; &infin;"))
+    ,conditionalPanel(
       condition= "input.alpha1 == 'homo'",
     plotOutput("doublePlot", height = "auto", width = "auto"),
     plotOutput("barPlot", height = "auto", width = "auto") 
