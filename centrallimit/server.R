7072a06ffa68b1456de6fca3aaa677b884fe87e5
gemarchand <geraldine.marchand@uclouvain.be>
2014-05-22 16:20:55 +0200

    Add descr stat + automatic reset when change dist

--- centrallimit/server.R
+++ centrallimit/server.R
@@ -2,10 +2,13 @@ Sys.setlocale("LC_ALL", "fr_FR.UTF-8")#to be sure that accents in text will be a
 
 
 library(shiny)
+library(plotrix)
 
 
 shinyServer(function(input, output){ 
 
+  
+ y.delta<-0.1
   
  rv <- reactiveValues()  # Create a reactiveValues object, to let us use settable reactive values
   
@@ -14,6 +17,8 @@ shinyServer(function(input, output){
 
   rv$lastAction <- 'none'# To start out, lastAction == NULL, meaning nothing clicked yet
   
+  rv$lastDist <- " "
+   
   # An observe block for each button, to record that the action happened
   observe({
     if (input$takesample != 0) {
@@ -28,13 +33,7 @@ shinyServer(function(input, output){
      }
   })
   
- bimodalDistFunc <- function (n,cpct, m1, m2, sd1, sd2) {
-   y0 <- rlnorm(n,mean=m1, sd = sd1)
-   y1 <- rlnorm(n,mean=m2, sd = sd2)
-   
-   flag <- rbinom(n,size=1, prob=cpct)
-   y <- y0*(1 - flag) + y1*flag}
- 
+
   getSamples<-reactive({
     if(input$takesample > rv$last.takesample.value && rv$lastAction == "takesample"){
       return(isolate({#Now do the expensive stuff
@@ -96,18 +95,12 @@ shinyServer(function(input, output){
        
     v<-getInputValues() # get all values of input list
     cv<-list()#created empty computed values list
-  
+    cv$samples.x<-list()
+    cv$n.samples<-length(rv$samples.z)
+    
+    
     
     ## Computation of sample related values ##
-    
-    cv$samples.x<-list()
-    cv$samples.x.m<-list()
-    cv$samples.x.sd<-list()
-    
-    
-    cv$n.samples<-length(rv$samples.z)
-      
-    
     if(cv$n.samples>0){
       for(i in 1:cv$n.samples){
         if (input$dist == "DN"){cv$samples.x[[i]]<-round((rv$samples.z[[i]]*v$sx)+v$mx,2)}
@@ -116,120 +109,178 @@ shinyServer(function(input, output){
         if (input$dist == "DC"){cv$samples.x[[i]]<-round(rv$samples.z[[i]], 2)}
         if (input$dist == "DF"){cv$samples.x[[i]]<-round(rv$samples.z[[i]], 2)}        
         if (input$dist == "DB"){cv$samples.x[[i]]<-round(rv$samples.z[[i]], 2)}
-        cv$samples.x.m[[i]]<-round(mean(cv$samples.x[[i]]),2)
-        cv$samples.x.sd[[i]]<-round(sd(cv$samples.x[[i]]),4)}
+       }
   
-
-    ## Last takesample value
-    rv$last.takesample.value<-v$takesample
-  return(cv)
+      if (rv$lastDist!=v$dist) {
+        rv$lastAction <- 'changeDist'
+        rv$last.takesample.value<-0
+        rv$samples.z <- list()
+        cv$samples.x <- list()
+      }
       
-  }
+      ##Pour passer d'une liste à une matrice
+      cv$samples.x.mat <- matrix(nrow = cv$n.samples, ncol = v$n) 
+      for(i in 1:cv$n.samples){
+        cv$samples.x.mat[i,] <- cv$samples.x[[i]]
+      }
+      
+      ## Computation of descriptives
+      cv$samples.x.m.vec<-c() # vector of mean values, each line a sample
+      cv$samples.x.sd.vec<-c()
+      cv$samples.x.m.vec<-round(apply(cv$samples.x.mat,1,mean),2)#means of samples
+      cv$samples.x.sd.vec<-round(apply(cv$samples.x.mat,1,sd),2)#sds of samples
+      
+      ## Define subset to plot
+      cv$samples.x.n.toshow<-0
+      cv$samples.x.from<-1
+      if(cv$n.samples>5){
+          cv$samples.x.from<-cv$n.samples-5+1
+        }    
+      cv$samples.x.to<-cv$n.samples
+        
+      cv$samples.x.mat.toshow<-matrix(cv$samples.x.mat[cv$samples.x.from:cv$samples.x.to,],ncol=v$n)
+      cv$samples.x.m.vec.toshow<-cv$samples.x.m.vec[cv$samples.x.from:cv$samples.x.to]
+      cv$samples.x.sd.vec.toshow<-cv$samples.x.sd.vec[cv$samples.x.from:cv$samples.x.to]
+      cv$samples.x.i.vec.toshow<-c(cv$samples.x.from:cv$samples.x.to)
+      cv$samples.x.n.toshow<-length(cv$samples.x.mat.toshow[,1])
+      cv$samples.y.mat.toshow<-c()
+        
+        if(cv$samples.x.n.toshow>0){
+        cv$samples.y.mat.toshow<-matrix(rep(y.delta/(5+1)*c(1:cv$samples.x.n.toshow),nrow=length(cv$samples.x.mat.toshow[,1])))
+        }
+      }  
+      
+      ## Last takesample value
+      rv$last.takesample.value<-v$takesample
+      rv$lastDist<-v$dist
+      return(cv)
     
   })
-    
-output$doublePlot <- renderPlot({
+ 
+ 
+ output$doublePlot <- renderPlot({
+   
+ v <- getInputValues ()
+ cv <- getComputedValues ()
+   
+ par(mfcol = c(2,2))
+ m<-matrix(c(1,2,3,4),2,2,byrow=TRUE)
+ layout(m)
+   
 
-v <- getInputValues ()
-cv <- getComputedValues ()
-  
-samples.obs <- unlist(cv$samples.x)
-
-samples.m <-unlist(cv$samples.x.m)
-
-par(mfcol = c(2,2))
-m<-matrix(c(1,2,3,4),2,2,byrow=TRUE)
-layout(m)
-
-
-
-######Output 1 : THEORETICAL DISTRIBUTION#####
-
-getY <-reactive({
-  n <- 1000000
-  if (input$dist=="DN"){
-    set.seed(1)
-    normData<-rnorm(n, mean=input$mx, sd=input$sx)
-    d<-density(normData)}
-  
-  if (input$dist=="DU"){
-    set.seed(1)
-    unifData<-runif(n, min=0, max=input$b)
-    d<-density(unifData)}
-  
-  if (input$dist=="DE"){
-    set.seed(1)
-    expData<-rexp(n, rate=input$Lambda)
-    d<-density(expData)}
-  
-  if (input$dist=="DC"){
-    set.seed(1)
-    chisqData<-rchisq(n, df=input$df)
-    d<-density(chisqData)}
-  
-  if (input$dist == "DF"){
-    set.seed(1)
-    fishData<-rf(n, df1=input$df1, df2=input$df2)
-    d<-density(fishData)}
-  
-  if (input$dist == "DB"){
-    set.seed(1)
-    bimodalData<- c(rnorm(n/2,input$m1, input$sd1), rnorm(n/2, input$m2, input$sd2))
-    d <- density(bimodalData)}
-  return (d)
-})
-
-
-dens <- getY()
-plot(dens, xlab="",bty="n",main=HTML("Distribution théorique"),type='l')
-
+ ######Output 1 : THEORETICAL DISTRIBUTION#####
+ 
+ if(input$dist=="DN"){x.lim.inf<-min(input$rangeXdn)
+                      x.lim.sup<-max(input$rangeXdn)}
+ if(input$dist=="DU"){x.lim.inf<-min(input$rangeXdu)
+                      x.lim.sup<-max(input$rangeXdu)}
+ if(input$dist=="DE"){x.lim.inf<-min(input$rangeXde)
+                      x.lim.sup<-max(input$rangeXde)}
+ if(input$dist=="DC"){x.lim.inf<-min(input$rangeXdc)
+                      x.lim.sup<-max(input$rangeXdc)}
+ if(input$dist=="DF"){x.lim.inf<-min(input$rangeXdf)
+                      x.lim.sup<-max(input$rangeXdf)}
+  if(input$dist=="DB"){x.lim.inf<-min(input$rangeXdb)
+                      x.lim.sup<-max(input$rangeXdb)}
+ 
+ ###Définition des X conditionnellement à la distribution 
+ 
+ if(input$dist=="DN"){X = seq(-10,40, length = 1000)}
+ if(input$dist=="DU"){X = seq(-5,25, length = 1000)}
+ if(input$dist=="DE"){X = seq(-5,5, length = 1000)}
+ if(input$dist=="DC"){X = seq(-5,60, length = 1000)}
+ if(input$dist=="DF"){X = seq(-5,10, length = 1000)}
+ 
+ 
+ getY <-reactive({
+   if (input$dist == "DN")
+     return(dnorm(X, mean=input$mx, sd=input$sx))
+   if (input$dist == "DU")
+     return(dunif (X, min=0, max=input$b))
+   if (input$dist == "DE")
+     return (dexp(X, rate=input$Lambda))
+   if (input$dist == "DC")
+     return (dchisq(X, df=input$df))
+   if (input$dist == "DF")
+     return(df(X,df1=input$df1, df2=input$df2))
+   if (input$dist == "DB")
+     return(density(c(rnorm(1000000/2,input$m1, input$sd1), rnorm(1000000/2, input$m2, input$sd2))))
+ })
+ 
+ 
+ if (input$dist == "DB"){
+   par(mai=c(0.5,0.5,0.5,0.5))
+   dens <- getY()
+   plot(dens,xlab="",bty="n",main=HTML("Distribution théorique"),type="l", xlim = c(x.lim.inf, x.lim.sup))
+ }
+ else {
+   Y<-getY()
+   par(mai=c(0.5,0.5,0.5,0.5))
+   plot(X,Y,xlab="",bty="n",main = HTML("Distribution théorique"),type = "l", xlim = c(x.lim.inf, x.lim.sup))  
+ }
+ 
+ 
 
 ###### Output 2 : statistiques descriptives #####
 
-if(is.null(samples.obs )){
+if(is.null(cv$samples.x.mat)){
+  par(mai=c(0.5,0.5,0.5,0.5))
   plot(c(0),c(0),xlab="",ylab="",xaxt="n",yaxt="n",bty="n",xlim=c(0,1),ylim=c(0,1),type='l')
+  mtext(bquote(paste("Descriptives : ", N == .(0), sep="")),side=1,line=1,at=0.05,adj=0)
 }
 
 else{ 
-  plot(c(0),c(0),xlab="",ylab="",xaxt="n",yaxt="n",bty="n",xlim=c(0,1),ylim=c(0,1),type='l')  
+  par(mai=c(0.5,0.5,0.5,0.5))
+  plot(c(0,1),c(0,0),col="white",xaxt="n",yaxt="n",xlab="",ylab="",ylim=c(0,0.05*2.1),bty="n",las=1)
+ 
+if(cv$samples.x.n.toshow>0){
+    for(i in 1:cv$samples.x.n.toshow){
+      text(0,cv$samples.y.mat.toshow[i,1],labels=bquote(paste(bar(x)[.(cv$samples.x.i.vec.toshow[i])] == .(sprintf("%.2f",cv$samples.x.m.vec.toshow[i])),sep="")),col="blue",pos=4, cex=1.3) #,cex=cex.samples
+      text(0.3,cv$samples.y.mat.toshow[i,1],labels=bquote(paste(s[.(cv$samples.x.i.vec.toshow[i])] == .(sprintf("%.2f",cv$samples.x.sd.vec.toshow[i])),sep="")),pos=4, cex=1.3) #,cex=cex.samples
+                                   }
+                           }
+mtext(bquote(paste("Descriptives : ", N == .(cv$n.samples), sep="")),side=1,line=1,at=0.05,adj=0)
 }  
 
 
 ###### Output 3 : HIST SAMPLE OBSERVATIONS #####
 
-if(is.null(samples.obs)){
+if(is.null(cv$samples.x.mat)){
   Y <- c()
   X <-c()
-  plot(X, Y, main=HTML("Histogramme des données d'échantillonnage"), xlim = c(0,20), ylim = c(0,10), xlab ="", bty="n", ylab = "") 
-  mtext(bquote(k == .(0)), side = 3, adj = 0, cex = 1)#afficher le nombre d'échantillons
-}
+  par(mai=c(0.5,0.5,0.5,0.5))
+  plot(X, Y, main=HTML("Histogramme des données d'échantillonnage"), xlim = c(x.lim.inf, x.lim.sup), ylim = c(0,10), xlab ="", bty="n", ylab = "") 
+ }
 else{
-  hist(samples.obs, freq = TRUE, xlab = "",breaks = 50, col = 'grey',main = HTML("Histogramme des données d'échantillonnage"))
-  mtext(bquote(k == .(cv$n.samples)), side = 3, adj = 0, cex = 1.2)
+  par(mai=c(0.5,0.5,0.5,0.5))
+  hist(cv$samples.x.mat, freq = TRUE, xlab = "",breaks = 50, col = 'grey',main = HTML("Histogramme des données d'échantillonnage"), xlim = c(x.lim.inf, x.lim.sup))
 }        
 
 
 ##### Output 4 : HIST SAMPLE MEANS#######
 
-if(is.null(samples.obs)){
+if(is.null(cv$samples.x.mat)){
   Y <- c()
   X <-c()
-  plot(X, Y, main=HTML("Histogramme des moyennes d'échantillonnage"), xlim = c(0,20), ylim = c(0,10), xlab ="", ylab = "", bty="n") 
-  mtext(bquote(k == .(0)), side = 3, adj = 0, cex = 1)
+  par(mai=c(0.5,0.5,0.5,0.5))
+  plot(X, Y, main=HTML("Histogramme des moyennes d'échantillonnage"), xlim = c(x.lim.inf, x.lim.sup), ylim = c(0,10), xlab ="", ylab = "", bty="n") 
+  
 }
 else{
-  h<-hist(samples.m, freq = TRUE, breaks=seq(0,20,0.1), xlab ="", main=HTML("Histogramme des moyennes d'échantillonnage"), col = 'grey')
+  par(mai=c(0.5,0.5,0.5,0.5))
+  h<-hist(cv$samples.x.m.vec, freq = TRUE, breaks=seq(x.lim.inf, x.lim.sup,0.1), xlab ="", main=HTML("Histogramme des moyennes d'échantillonnage"), col = 'grey', xlim = c(x.lim.inf, x.lim.sup))
 }   
 
 #afficher la densité normale sur l'histogramme (option)  
-  if(input$showNdensity && !is.null(cv$n.samples)){  
-  lim_inf <- min (samples.m)-1
-  lim_sup <- max(samples.m)+1
+if(input$showNdensity && !is.null(cv$samples.x.mat)){  
+  lim_inf <- min (cv$samples.x.m.vec)-1
+  lim_sup <- max(cv$samples.x.m.vec)+1
   xfit<-seq(lim_inf,lim_sup,length=100) 
-  yfit<-dnorm(xfit,mean=mean(samples.m),sd=sd(samples.m))
-  yfit <- yfit*diff(h$mids[1:2])*length(samples.m) 
+  yfit<-dnorm(xfit,mean=mean(cv$samples.x.m.vec),sd=sd(cv$samples.x.m.vec))
+  yfit <- yfit*diff(h$mids[1:2])*length(cv$samples.x.m.vec) 
   lines(xfit, yfit, col="blue", type = 'l',lwd=2)
 }
+   else{}
 
         
        },height = getPlotHeight, width=getPlotWidth)
--- centrallimit/ui.R
+++ centrallimit/ui.R
@@ -23,7 +23,7 @@ shinyUI(pageWithSidebar(
             ,h5(HTML("Paramètres de l'échantillonnage"))
             ,sliderInput("n",HTML("Nombre d'individus par échantillon : n"),min = 5,max = 200,value =50, step=5)
             ,sliderInput("ns",HTML("Nombre d'échantillons prélevés par simulation"), min = 1, max = 50, value = 1)
-            ,p(HTML("k : Nombre total d'échantillons prélevés"))
+            ,p(HTML("N : Nombre total d'échantillons prélevés"))
             
             ,h5(HTML("Paramètres de la distribution théorique"))
             ,selectInput("dist", HTML("Sélectionner le type de distribution :"),
@@ -36,33 +36,43 @@ shinyUI(pageWithSidebar(
             
 #Normale
             conditionalPanel(condition = "input.dist == 'DN'" 
-              ,p (HTML("X&sim;&Nu;(&mu;, &sigma;<sup>2</sup>)"))
+              ,p(HTML("X&sim;&Nu;(&mu;, &sigma;<sup>2</sup>)"))
               ,HTML("&mu;")               
               ,sliderInput("mx","" , min = 5, max = 15, value = 10, step = 0.1) 
               ,HTML("&sigma;")          
-              ,sliderInput("sx","", min = 0.5, max = 3.5, value = 2, step = 0.1))
+              ,sliderInput("sx","", min = 0.5, max = 3.5, value = 2, step = 0.1)
+              ,sliderInput("rangeXdn", HTML("Choix de l'étendue en abscisse"),
+                                         min = -10, max = 40, value = c(0,20)))
 #Uniforme          
           ,conditionalPanel(condition = "input.dist == 'DU'" 
-             ,p (HTML("X&sim;U(&theta;<sub>1</sub>, &theta;<sub>2</sub>) &nbsp;&nbsp;avec &theta;<sub>1</sub>=0"))
+             ,p(HTML("X&sim;U(&theta;<sub>1</sub>, &theta;<sub>2</sub>) &nbsp;&nbsp;avec &theta;<sub>1</sub>=0"))
              ,HTML("&theta;<sub>2</sub>") 
-             ,sliderInput("b", "", min = 1, max = 20, value = 10, step = 0.1))
+             ,sliderInput("b", "", min = 1, max = 20, value = 20, step = 0.1)
+             ,sliderInput("rangeXdu", HTML("Choix de l'étendue en abscisse"),
+                         min = -5, max = 25, value = c(0,20)))
 #Exponentielle          
           ,conditionalPanel(condition = "input.dist == 'DE'" 
-             ,p (HTML("X&sim;&Epsilon;(&lambda;)"))
+             ,p(HTML("X&sim;&Epsilon;(&lambda;)"))
              ,HTML("&lambda;") 
-             ,sliderInput("Lambda", "", min = 0.1, max = 10, value = 2, step = 0.1))
+             ,sliderInput("Lambda", "", min = 0.1, max = 10, value = 2, step = 0.1)
+             ,sliderInput("rangeXde", HTML("Choix de l'étendue en abscisse"),
+                                         min = -5, max = 5, value = c(0,5)))
 #Chi-carrée         
           ,conditionalPanel(condition = "input.dist == 'DC'"
-             ,p (HTML("X&sim;&Chi;<sup>2</sup><sub>&nu;</sub>"))
+             ,p(HTML("X&sim;&Chi;<sup>2</sup><sub>&nu;</sub>"))
              ,HTML("&nu;") 
-             ,sliderInput("df", "", min = 1, max = 20, value = 5, step = 1))
+             ,sliderInput("df", "", min = 1, max = 20, value = 5, step = 1)
+             ,sliderInput("rangeXdc", HTML("Choix de l'étendue en abscisse"),
+                                         min = -5, max = 60, value = c(0,20)))
 #Fisher          
           ,conditionalPanel(condition = "input.dist == 'DF'" 
-             ,p (HTML("X&sim;F<sub>&nu;<sub>1</sub></sub>,<sub>&nu;<sub>2</sub></sub>"))
+             ,p(HTML("X&sim;F<sub>&nu;<sub>1</sub></sub>,<sub>&nu;<sub>2</sub></sub>"))
              ,HTML("&nu;<sub>1</sub>") 
              ,sliderInput("df1", "", min = 1, max = 50, value = 5, step = 1) 
              ,HTML("&nu;<sub>2</sub>") 
-             ,sliderInput("df2", "", min = 1, max = 100, value = 20, step = 1))
+             ,sliderInput("df2", "", min = 1, max = 100, value = 20, step = 1)
+             ,sliderInput("rangeXdf", HTML("Choix de l'étendue en abscisse"),
+                         min = -5, max = 10, value = c(0,5)))
 #Bimodale
           ,conditionalPanel(condition = "input.dist == 'DB'" 
              ,HTML("&mu;<sub>1</sub>")                
@@ -72,11 +82,11 @@ shinyUI(pageWithSidebar(
              ,HTML("&sigma;<sub>1</sub>")  
              ,sliderInput("sd1", "", min = 1, max = 2, value = 1.5, step = 0.01)
              ,HTML("&sigma;<sub>2</sub>")  
-             ,sliderInput("sd2", "", min = 1, max = 2, value = 1.1, step = 0.01)                             
-            )
+             ,sliderInput("sd2", "", min = 1, max = 2, value = 1.1, step = 0.01)  
+             ,sliderInput("rangeXdb", HTML("Choix de l'étendue en abscisse"),
+                                         min = -20, max = 40, value = c(0,15)))
             
-
-           
+            
          ,checkboxInput("showNdensity", HTML("Visualiser la densité normale pour la distribution d'échantillonnage de la moyenne"), FALSE)
               
          ,h5("Paramètres graphiques :"),
